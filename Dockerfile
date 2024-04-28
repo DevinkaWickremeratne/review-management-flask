e8636de8b226c84348618c05c5026eaf79ee38d5
Devinka Wickremeratne <devinkamw5250@gmail.com>
2024-04-28 09:54:41 +0530

    refactor(app): refactor application

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,21 @@
-FROM python:alpine
+FROM python:alpine AS production
 
 WORKDIR /python-docker
 
+# Copy only the necessary files for production
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
 COPY . .
-RUN ls
+
 # Create a new user with UID 10016
 RUN addgroup -g 10016 choreo && \
     adduser  --disabled-password  --no-create-home --uid 10016 --ingroup choreo choreouser
+
+# Switch to the new user
 USER 10016
+
 EXPOSE 5000
-CMD [ "flask", "run", "--host=0.0.0.0"]
+
+# Start the Flask application
+CMD ["flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_restx import Api
+from models import Conference as ConferenceModel,Review
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-api = Api(app, version='1.0', title='Your API Title', description='Your API Description')
 
-from routes import *
+@app.route('/conferences', methods=['GET'])
+def get_conferences():
+    conferences = ConferenceModel.query.all()
+    return jsonify([conference.serialize() for conference in conferences])
+
+@app.route('/conferences/<int:conference_id>', methods=['GET'])
+def get_conference(conference_id):
+    conference = ConferenceModel.query.get(conference_id)
+    if not conference:
+            return jsonify({'message': 'Conference not found'}), 404
+    return jsonify(conference.serialize())
+
+@app.route('/conferences/<int:conference_id>/reviews', methods=['GET'])
+def get_reviews_for_conference(conference_id):
+    reviews = Review.query.filter_by(conference_id=conference_id).all()
+    return jsonify([review.serialize() for review in reviews])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
